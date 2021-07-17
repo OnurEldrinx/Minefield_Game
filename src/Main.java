0b746e533f3ff4c5a9cbd0ab57047f50dc0fea55
Onur Oz <onureldrinx@gmail.com>
2021-07-18 01:34:37 +0300

    Input from user

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+        int x,y;
 
         System.out.println("\n***************  Welcome to the MineField  ***************\n");
 
-        MineSweeper game = new MineSweeper(3,3);
+        System.out.print("Enter the number of rows    : ");
+        x = scanner.nextInt();
+        System.out.print("Enter the number of columns : ");
+        y = scanner.nextInt();
+
+        System.out.println();
+
+        MineSweeper game = new MineSweeper(x,y);
 
         MineSweeper.run(game);
 
